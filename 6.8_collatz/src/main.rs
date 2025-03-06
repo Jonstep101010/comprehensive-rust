b19c68639ff143a1fd9297a95ab4601373f99e0c
Jonstep101010 <git@jschwabe.site>
2025-03-06 18:58:01 +0100

    chore: collatz
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 6.8_collatz/src/main.rs
+++ 6.8_collatz/src/main.rs
@@ -22,7 +22,12 @@ Write a function to calculate the length of the collatz sequence for a given ini
 
 /// Determine the length of the collatz sequence beginning at `n`.
 fn collatz_length(mut n: i32) -> u32 {
-	todo!("Implement this")
+	let mut len = 1;
+	while n > 1 {
+		n = if n % 2 == 0 { n / 2} else { 3 * n + 1};
+		len += 1;
+	}
+	len
 }
 
 fn main() {
