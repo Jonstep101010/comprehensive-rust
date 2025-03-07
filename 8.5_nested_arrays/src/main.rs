50fc18accdb9e358ac775f51fc1149b7339371bb
Jonstep101010 <git@jschwabe.site>
2025-03-07 18:07:08 +0100

    exercise: nested_arrays
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 8.5_nested_arrays/src/main.rs
+++ 8.5_nested_arrays/src/main.rs
@@ -3,7 +3,14 @@
 ///
 /// matrix first row will be [101, 201, 301]
 fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
-	todo!()
+	let mut transposed_matrix = [[0; 3]; 3];
+
+	for row in 0..3 {
+		for col in 0..3 {
+			transposed_matrix[col][row] = matrix[row][col];
+		}
+	}
+	transposed_matrix
 }
 
 fn main() {
