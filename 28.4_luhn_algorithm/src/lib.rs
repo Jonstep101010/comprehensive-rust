ce2c7d9fd81c9a18af1f78c8f9c4c23450d74b5a
Jonstep101010 <git@jschwabe.site>
2025-03-13 18:11:48 +0100

    chore: complete exercise luhn_algorithm

--- 28.4_luhn_algorithm/src/lib.rs
+++ 28.4_luhn_algorithm/src/lib.rs
@@ -1,5 +1,6 @@
 /*
-The Luhn algorithm is used to validate credit card numbers. The algorithm takes a string as input and does the following to validate the credit card number:
+The Luhn algorithm is used to validate credit card numbers.
+The algorithm takes a string as input and does the following to validate the credit card number:
 
 	Ignore all spaces. Reject numbers with fewer than two digits.
 
@@ -17,9 +18,11 @@ The provided code provides a buggy implementation of the luhn algorithm, along w
 pub fn luhn(cc_number: &str) -> bool {
 	let mut sum = 0;
 	let mut double = false;
+	let mut digits = 0;
 
 	for c in cc_number.chars().rev() {
 		if let Some(digit) = c.to_digit(10) {
+			digits += 1;
 			if double {
 				let double_digit = digit * 2;
 				sum += if double_digit > 9 {
@@ -31,12 +34,15 @@ pub fn luhn(cc_number: &str) -> bool {
 				sum += digit;
 			}
 			double = !double;
-		} else {
+		} else if c.is_whitespace() && digits >= 3 {
+			// ignore whitespace, but only if it is not trailing
 			continue;
+		} else {
+			return false;
 		}
 	}
 
-	sum % 10 == 0
+	digits >= 2 && sum % 10 == 0
 }
 
 #[cfg(test)]
@@ -55,5 +61,28 @@ mod test {
 		assert!(!luhn("4223 9826 4026 9299"));
 		assert!(!luhn("4539 3195 0343 6476"));
 		assert!(!luhn("8273 1232 7352 0569"));
+		assert!(!luhn("8273 0569"));
+	}
+	#[test]
+	fn test_invalid_length_cc_number() {
+		assert!(!luhn("1234 5678 9012 3456 7890"));
+		assert!(!luhn("0"));
+		assert!(!luhn(" 0"));
+		assert!(!luhn("   0    "));
+		assert!(!luhn("0 0 "));
+		assert!(!luhn("   0   0    "));
+		assert!(!luhn("   0   0"));
+	}
+	#[test]
+	fn test_nondigit_cc_number() {
+		assert!(!luhn("some text"));
+		assert!(!luhn("4223 9826 a026 9299"));
+	}
+	#[test]
+	fn test_empty_cc_number() {
+		assert!(!luhn(""));
+		assert!(!luhn("  "));
+		assert!(!luhn("     "));
+		assert!(!luhn("     	"));
 	}
 }
